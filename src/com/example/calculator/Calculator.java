d3952f672f345dd5585516679565427ebccebf46
t2025-m0237 <asd@asad>
2026-06-17 19:57:23 +0900

    사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -7,4 +7,27 @@ public class Calculator {
 
     public List<Integer> results = new ArrayList<>();
 
+    public Integer caculate(int num1, int num2, char operator) {
+
+        int result;
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return;
+                }
+                result = num1 / num2;
+            }
+            default -> {
+                System.out.println("지원하지 않는 연산자입니다.");
+                return;
+            }
+        }
+
+    }
+
 }
